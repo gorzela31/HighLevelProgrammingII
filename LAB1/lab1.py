8bf501845f145345c4b20bca7139ee3b62d13ddb
Bartłomiej Gorzela <ul0288785@edu.uni.lodz.pl>
2025-03-16 09:54:42 +0100

    Powort do danych o jednym z obiektow w pliku json

--- LAB1/lab1.py
+++ LAB1/lab1.py
@@ -7,12 +7,12 @@ object3 = model_ai.ModelAI("Name3",3.0)
 
 print(object1.ile_modeli())
 
-#objectFromFile = model_ai.ModelAI.z_pliku("model.json")
+objectFromFile = model_ai.ModelAI.z_pliku("model.json")
 
-#print(object1.ile_modeli())
-#print(objectFromFile.modelName)
+print(object1.ile_modeli())
+print(objectFromFile.modelName)
 
-with open("model.json", "r") as f:
-    text = json.load(f)
-    print(text[0]["name"])
-    print(len(text))
+# with open("model.json", "r") as f:
+#     text = json.load(f)
+#     print(text[0]["name"])
+#     print(len(text))
