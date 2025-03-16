921f984e6090e03fee0877e440938cd187283b3f
Bartłomiej Gorzela <ul0288785@edu.uni.lodz.pl>
2025-03-16 09:51:08 +0100

    zadanie 1 z zajec

---
+++ LAB1/lab1.py
@@ -0,0 +1,18 @@
+import model_ai
+import json
+
+object1 = model_ai.ModelAI("Name1",1.0)
+object2 = model_ai.ModelAI("Name2",2.0)
+object3 = model_ai.ModelAI("Name3",3.0)
+
+print(object1.ile_modeli())
+
+#objectFromFile = model_ai.ModelAI.z_pliku("model.json")
+
+#print(object1.ile_modeli())
+#print(objectFromFile.modelName)
+
+with open("model.json", "r") as f:
+    text = json.load(f)
+    print(text[0]["name"])
+    print(len(text))
